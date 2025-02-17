ee150d70583dfeaa77d018f1541ad58cd4ef701b
jungwon <wjddnjs0528@gmail.com>
2025-02-17 22:56:26 +0900

    ✨: topbar 탑재 방식 변경

--- app/src/main/kotlin/com/hm/picplz/ui/screen/detail_photographer/DetailPhotographerScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/detail_photographer/DetailPhotographerScreen.kt
@@ -1,23 +1,35 @@
 package com.hm.picplz.ui.screen.detail_photographer
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.hm.picplz.MainActivity
 import com.hm.picplz.ui.screen.common.CommonBottomButton
 import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.theme.MainThemeColor
@@ -29,27 +41,35 @@ fun DetailPhotographerScreen(navController: NavHostController) {
     val paddingModifier = Modifier.padding(horizontal = 15.dp)
     val scrollState = rememberScrollState()
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        containerColor = MainThemeColor.White,
-        topBar = {
-            CommonTopBar(text = "", onClickBack = { /*TODO*/ })
-        },
-        floatingActionButton = {
-            Box(modifier = Modifier.padding(start = 30.dp)) {
-                CommonBottomButton(
-                    text = "예약하기",
-                    onClick = { },
-                    containerColor = MainThemeColor.Black,
+    Scaffold(modifier = Modifier.fillMaxSize(), containerColor = MainThemeColor.White, topBar = {
+    }, floatingActionButton = {
+        Box(modifier = Modifier.padding(start = 30.dp)) {
+            CommonBottomButton(
+                text = "예약하기",
+                onClick = { },
+                containerColor = MainThemeColor.Black,
+            )
+        }
+    }, content = { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .background(MainThemeColor.White) // 배경을 주어 내용이 비치지 않도록
+                    .zIndex(1f) // 항상 위쪽에 있도록 설정
+                    .height(56.dp)
+            ) {
+                CommonTopBar(
+                    text = "ㅇㅇㅇㅇㅇ",
+                    onClickBack = { /*TODO*/ }
                 )
             }
-        },
-        content = { innerPadding ->
+
             Column(
                 modifier = Modifier
-                    .fillMaxWidth() // fillMaxWidth로 너비를 맞추고
+                    // fillMaxWidth로 너비를 맞추고
                     .verticalScroll(rememberScrollState()) // verticalScroll을 적용
-                    .padding(innerPadding) // innerPadding을 사용
             ) {
                 DetailProfileSection(modifier = paddingModifier)
 
@@ -74,7 +94,7 @@ fun DetailPhotographerScreen(navController: NavHostController) {
                 Spacer(modifier = Modifier.height(130.dp))
             }
         }
-    )
+    })
 }
 
 @Preview(showBackground = true)
