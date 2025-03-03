2a9a9b5c488715094f0b8f0bd5d6b8b94d45df64
ldhldh07 <dhlim07@naver.com>
2025-03-03 18:00:19 +0900

    :sparkles: : 기본 파일 추가

---
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/photographer_main/PhotographerMainScreen.kt
@@ -0,0 +1,7 @@
+package com.hm.picplz.ui.screen.photographer_main
+
+import androidx.compose.runtime.Composable
+import com.hm.picplz.ui.model.Photographer
+
+@Composable
+fun PhotographerMainScreen() {}
