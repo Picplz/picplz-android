45c9a1ae5f42915a4336da5b0614f6571ec0730c
jungwon <wjddnjs0528@gmail.com>
2025-03-12 23:40:21 +0900

    ✨: 하단 네비게이션바 윤곽선 그림자 추가

--- app/src/main/kotlin/com/hm/picplz/navigation/bottom_navigation/BottomNavigationBar.kt
+++ app/src/main/kotlin/com/hm/picplz/navigation/bottom_navigation/BottomNavigationBar.kt
@@ -1,7 +1,6 @@
 package com.hm.picplz.navigation.bottom_navigation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
@@ -9,15 +8,17 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
@@ -51,9 +52,14 @@ fun BottomNavigationBar(navController: NavHostController, userType: UserType = U
     val currentDestination = navBackStackEntry?.destination
 
     NavigationBar(
-        containerColor = Color.White, // backgroundColor -> containerColor
-        contentColor = Color.Black,
-        modifier = Modifier.height(84.dp),
+        containerColor = MainThemeColor.White, // backgroundColor -> containerColor
+        contentColor = MainThemeColor.Black,
+        modifier = Modifier
+            .height(84.dp)
+            .shadow(
+                elevation = 12.dp,
+                shape = RoundedCornerShape(0.dp),
+            )
     ) {
         items.forEach { item ->
             AddItem(
@@ -71,13 +77,11 @@ fun RowScope.AddItem(
 ) {
     val isSelected = currentDestination?.route == item.route
 
-    NavigationBarItem(
-        selected = isSelected,
+    NavigationBarItem(selected = isSelected,
         onClick = {}, // 클릭 이벤트 제거(RippleEffect 제거)
-        modifier = Modifier.background(MainThemeColor.Transparent),
+        colors = NavigationBarItemDefaults.colors(indicatorColor = MainThemeColor.Transparent),
         icon = {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .clickable(
                         interactionSource = remember { MutableInteractionSource() }, // RippleEffect 제거
@@ -88,9 +92,8 @@ fun RowScope.AddItem(
                             restoreState = true
                         }
                     }
-                    .size(100.dp)
-            ) {
-                Spacer(modifier = Modifier.height(30.dp))
+                    .size(100.dp)) {
+                Spacer(modifier = Modifier.height(15.dp))
                 Image(
                     painter = painterResource(id = if (isSelected) item.iconSelect else item.iconUnselect),
                     contentDescription = "icon",
@@ -103,6 +106,5 @@ fun RowScope.AddItem(
                     color = if (isSelected) MainThemeColor.Black else MainThemeColor.Gray3
                 )
             }
-        }
-    )
+        })
 }
