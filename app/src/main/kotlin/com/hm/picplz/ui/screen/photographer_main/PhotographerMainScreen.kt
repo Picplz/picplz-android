6fa688dead6108bf0fde4e988b6301eb24a300dc
ldhldh07 <dhlim07@naver.com>
2025-03-20 16:56:20 +0900

    :lipstick: : 작가 검색 페이지 지도 부분 ui

--- app/src/main/kotlin/com/hm/picplz/ui/screen/photographer_main/PhotographerMainScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/photographer_main/PhotographerMainScreen.kt
@@ -1,21 +1,27 @@
 package com.hm.picplz.ui.screen.photographer_main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.hm.picplz.R
 import com.hm.picplz.navigation.bottom_navigation.BottomNavigationBar
 import com.hm.picplz.ui.screen.common.AddressMarker
 import com.hm.picplz.ui.screen.common.RefetchButton
@@ -41,7 +47,10 @@ fun PhotographerMainScreen(
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            Box {
+            Box (
+                Modifier
+                    .background(color = MainThemeColor.Gray1)
+            ){
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -54,6 +63,22 @@ fun PhotographerMainScreen(
                     )
                     RefetchButton(
                         onClick = { }
+                    )
+                }
+                Box(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(370.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.double_circle),
+                        contentDescription = "범위 이미지",
+                        modifier = Modifier
+                    )
+                    Image(
+                        painter = painterResource(R.drawable.searching_photographer),
+                        contentDescription = "작가 캐릭터",
                     )
                 }
             }
