082e71b23fc81e33d4a2cbf918a110d2f108d422
ldhldh07 <dhlim07@naver.com>
2025-03-20 15:46:29 +0900

    :lipstick: : 상단 주소 및 refetch 버튼 영역 - 기존 작가 검색 페이지 composable인 두 composable을 common으로 이동

--- app/src/main/kotlin/com/hm/picplz/ui/screen/photographer_main/PhotographerMainScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/photographer_main/PhotographerMainScreen.kt
@@ -1,14 +1,22 @@
 package com.hm.picplz.ui.screen.photographer_main
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.hm.picplz.navigation.bottom_navigation.BottomNavigationBar
+import com.hm.picplz.ui.screen.common.AddressMarker
+import com.hm.picplz.ui.screen.common.RefetchButton
 import com.hm.picplz.ui.theme.MainThemeColor
 
 @Composable
@@ -25,9 +33,27 @@ fun PhotographerMainScreen(
             .fillMaxSize()
             .systemBarsPadding()
     ) { innerPadding ->
-        Text(
-            text = "작가 지도 화면",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Box {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp, start = 5.dp, end = 3.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    AddressMarker(
+                        address = "마포구 서교동"
+                    )
+                    RefetchButton(
+                        onClick = { }
+                    )
+                }
+            }
+        }
     }
 }
