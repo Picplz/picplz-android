38b23eccb697646fb9ba096b75ad69b6e23f71c2
jungwon <wjddnjs0528@gmail.com>
2025-03-25 15:13:30 +0900

    ✨: CommonModalBottomSheet & CommonScrollPicker 적용하여 레이아웃 코드 수정 - CommonModalBottomSheet로 모달 바텀시트 구현 - CommonScrollPicker - SingleScrollPicker로 스크롤 가능한 선택기 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_photographer/views/SignUpCareerPeriodScreen.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
@@ -27,10 +32,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.hm.picplz.ui.screen.common.CommonBottomButton
-import com.hm.picplz.ui.screen.common.CommonNumberSelector
+import com.hm.picplz.ui.screen.common.CommonModalBottomSheet
 import com.hm.picplz.ui.screen.common.CommonTopBar
+import com.hm.picplz.ui.screen.common.SingleNumberScrollPicker
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SelectorType
-import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.*
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.InitializeCareerValues
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.Navigate
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.NavigateToPrev
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetCareerPeriod
+import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerIntent.SetSelectedSelector
 import com.hm.picplz.ui.screen.sign_up.sign_up_photographer.SignUpPhotographerSideEffect
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
@@ -60,7 +71,7 @@ fun SignUpCareerPeriodScreen(
         ) {
             CommonTopBar(
                 text = "경력 선택",
-                onClickBack = {viewModel.handleIntent(NavigateToPrev)}
+                onClickBack = { viewModel.handleIntent(NavigateToPrev) }
             )
             Box(
                 modifier = Modifier
@@ -172,25 +183,29 @@ fun SignUpCareerPeriodScreen(
         }
     }
 
-    CommonNumberSelector(
-        currentValue = currentState.yearValue,
-        onValueSelected = { year ->
-            viewModel.handleIntent(SetYearValue(year))
-            viewModel.handleIntent(SetSelectedSelector(SelectorType.NONE))
-        },
-        maxValue = 50,
+    fun closeBottomSheet() {
+        viewModel.handleIntent(SetSelectedSelector(SelectorType.NONE))
+    }
+
+    CareerModalBottomSheet(
+        onDismiss = { closeBottomSheet() },
         visible = currentState.selectedSelector === SelectorType.YEAR,
-        onDismiss = { viewModel.handleIntent(SetSelectedSelector(SelectorType.NONE)) }
-    )
-    CommonNumberSelector(
-        currentValue = currentState.monthValue,
-        onValueSelected = { month ->
-            viewModel.handleIntent(SetMonthValue(month))
-            viewModel.handleIntent(SetSelectedSelector(SelectorType.NONE))
+        initialNumber = currentState.yearValue ?: 0,
+        numberRange = 0..99,
+        onValueSelected = {
+            viewModel.handleIntent(SignUpPhotographerIntent.SetYearValue(it))
+            closeBottomSheet()
         },
-        maxValue = 12,
+    )
+    CareerModalBottomSheet(
+        onDismiss = { closeBottomSheet() },
         visible = currentState.selectedSelector === SelectorType.MONTH,
-        onDismiss = { viewModel.handleIntent(SetSelectedSelector(SelectorType.NONE)) }
+        initialNumber = currentState.monthValue ?: 0,
+        numberRange = 0..12,
+        onValueSelected = {
+            viewModel.handleIntent(SignUpPhotographerIntent.SetMonthValue(it))
+            closeBottomSheet()
+        },
     )
 
     LaunchedEffect(Unit) {
@@ -199,16 +214,51 @@ fun SignUpCareerPeriodScreen(
                 is SignUpPhotographerSideEffect.NavigateToPrev -> {
                     signUpPhotographerNavController.popBackStack()
                 }
+
                 is SignUpPhotographerSideEffect.Navigate -> {
                     signUpPhotographerNavController.navigate(sideEffect.destination)
                 }
+
                 else -> {}
             }
         }
     }
 }
 
-@Preview (showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CareerModalBottomSheet(
+    onDismiss: () -> Unit,
+    visible: Boolean,
+    initialNumber: Int,
+    numberRange: IntRange,
+    onValueSelected: (Int) -> Unit,
+) {
+    CommonModalBottomSheet(
+        onDismissRequest = onDismiss,
+        dragHandle = null,
+        visible = visible,
+        sheetMaxHeight = 380.dp
+    ) {
+        var selectedNum by remember { mutableIntStateOf(initialNumber) }
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            SingleNumberScrollPicker(
+                modifier = Modifier.padding(horizontal = 20.dp),
+                initialNumber = initialNumber,
+                numberRange = numberRange
+            ) {
+                selectedNum = it
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            CommonBottomButton(text = "확인", onClick = {
+                onValueSelected(selectedNum)
+            })
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun SignUpCareerPeriodScreenPreview() {
     PicplzTheme {
