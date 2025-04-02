d58becb70885878f4b1adf4dab9469ea1264c1b8
ldhldh07 <dhlim07@naver.com>
2025-04-02 17:08:34 +0900

    :lipstick: : 상태창 포함되어 중앙 정렬되는 점 조정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonButtonModal.tk.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonButtonModal.tk.kt
@@ -2,14 +2,19 @@ package com.hm.picplz.ui.screen.common
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -17,6 +22,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -27,6 +33,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.hm.picplz.R
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.pretendardTypography
@@ -45,73 +52,87 @@ fun CommonButtonModal(
     Dialog(
         onDismissRequest = onDismissRequest
     ){
-        Surface(
-            modifier = modifier
-                .width(width)
-                .border(
-                    width = 1.dp,
-                    color = MainThemeColor.Gray2,
-                    shape = RoundedCornerShape(5.dp)
-                )
-            ,
-            shape = RoundedCornerShape(5.dp),
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = (-30).dp)
+                .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) { onDismissRequest() },
+                contentAlignment = Alignment.Center
         ) {
-            Column {
-                content()
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(46.dp)
-                        .drawBehind {
-                            drawLine(
-                                color = MainThemeColor.Gray3,
-                                start = Offset(0f, 0f),
-                                end = Offset(size.width, 0f),
-                                strokeWidth = 1.dp.toPx()
+            Surface(
+                modifier = modifier
+                    .width(width)
+                    .border(
+                        width = 1.dp,
+                        color = MainThemeColor.Gray2,
+                        shape = RoundedCornerShape(5.dp)
+                    )
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) {},
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Column {
+                    content()
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(46.dp)
+                            .drawBehind {
+                                drawLine(
+                                    color = MainThemeColor.Gray3,
+                                    start = Offset(0f, 0f),
+                                    end = Offset(size.width, 0f),
+                                    strokeWidth = 1.dp.toPx()
+                                )
+                            }
+                    ) {
+                        Button(
+                            onClick = onCancel,
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight(),
+                            shape = RoundedCornerShape(
+                                topStart = 0.dp,
+                                topEnd = 0.dp,
+                                bottomStart = 5.dp,
+                                bottomEnd = 0.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MainThemeColor.White,
+                                contentColor = MainThemeColor.Black,
+                            )
+                        ) {
+                            Text(
+                                text = cancelText,
+                                style = pretendardTypography.titleSmall,
                             )
                         }
-                ) {
-                    Button(
-                        onClick = onCancel,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        shape = RoundedCornerShape(
-                            topStart = 0.dp,
-                            topEnd = 0.dp,
-                            bottomStart = 5.dp,
-                            bottomEnd = 0.dp
-                        ),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MainThemeColor.White,
-                            contentColor = MainThemeColor.Black,
-                        )
-                    ) {
-                        Text(
-                            text = cancelText,
-                            style = pretendardTypography.titleSmall,
-                        )
-                    }
-                    Button(
-                        onClick = onConfirm,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        shape = RoundedCornerShape(
-                            topStart = 0.dp,
-                            topEnd = 0.dp,
-                            bottomStart = 0.dp,
-                            bottomEnd = 5.dp
-                        ),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MainThemeColor.Black,
-                            contentColor = MainThemeColor.White,
-                        )
-                    ) {
-                        Text(
-                            text = confirmText,
-                            style = pretendardTypography.titleSmall
-                        )
+                        Button(
+                            onClick = onConfirm,
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight(),
+                            shape = RoundedCornerShape(
+                                topStart = 0.dp,
+                                topEnd = 0.dp,
+                                bottomStart = 0.dp,
+                                bottomEnd = 5.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MainThemeColor.Black,
+                                contentColor = MainThemeColor.White,
+                            )
+                        ) {
+                            Text(
+                                text = confirmText,
+                                style = pretendardTypography.titleSmall
+                            )
+                        }
                     }
                 }
             }
