1b59e91b408ccd8293a76d7a5593df7abf581857
ldhldh07 <dhlim07@naver.com>
2025-04-14 21:37:22 +0900

    :adhesive_bandage: :  더미 데이터 추가

--- app/src/main/kotlin/com/hm/picplz/ui/screen/chat/ChatState.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/chat/ChatState.kt
@@ -1,6 +1,8 @@
 package com.hm.picplz.ui.screen.chat
 
+import com.hm.picplz.ui.model.ChatRoomInfo
 import com.hm.picplz.ui.model.ChatStatus
+import com.hm.picplz.ui.model.Message
 
 enum class ChatTabType {
     ONGOING, COMPLETED
@@ -21,3 +23,42 @@ data class ChatState (
             ChatTabType.COMPLETED -> listOf(ChatStatus.REJECTED, ChatStatus.COMPLETED)
         }
 }
+
+val dummyChatRooms =
+    listOf(
+    ChatRoomInfo(
+        id = "chat1",
+        chatStatus = ChatStatus.PENDING,
+        packageType = "프로필 Only.",
+        lastMessage = Message(
+            profileImageUrl = "https://api.dicebear.com/7.x/avataaars/svg?seed=John",
+            nickname = "합정동 불주먹",
+            message = "촬영 예약이 도착했습니다. 60분 이내에 답변 안 할 시 예약이 취소될 수 있습니다.",
+            sentAt = System.currentTimeMillis() - (1000 * 60 * 3),
+        ),
+        unreadMessageCount = 1,
+    ),
+    ChatRoomInfo(
+        id = "chat1",
+        chatStatus = ChatStatus.CONFIRMED,
+        packageType = "인스타 종합 패키지",
+        lastMessage = Message(
+            profileImageUrl = "https://api.dicebear.com/7.x/avataaars/svg?seed=John",
+            nickname = "오늘저역방어",
+            message = "내일 14시에 뵐게요~~",
+            sentAt = System.currentTimeMillis() - (1000 * 60 * 48),
+        )
+    ),
+    ChatRoomInfo(
+        id = "chat1",
+        chatStatus = ChatStatus.CONFIRMED,
+        packageType = "인스타 종합 패키지",
+        lastMessage = Message(
+            profileImageUrl = "https://api.dicebear.com/7.x/avataaars/svg?seed=John",
+            nickname = "하배고파 ",
+            message = "주문서 쓰기 너무 기차나요ㅜ 좀만 기달",
+            sentAt = System.currentTimeMillis() - (1000 * 60 * 60),
+        ),
+        unreadMessageCount = 3
+    ),
+)
