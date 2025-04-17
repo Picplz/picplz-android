846ac47b05de07aa6728a9c3fa5d1545d8bcceeb
ldhldh07 <dhlim07@naver.com>
2025-04-17 16:07:50 +0900

    :lipstick: : 예약 단계 영역 ui 개발

--- app/src/main/kotlin/com/hm/picplz/ui/screen/chat/ChatRoomScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/chat/ChatRoomScreen.kt
@@ -1,15 +1,26 @@
 package com.hm.picplz.ui.screen.chat
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.hm.picplz.R
 import com.hm.picplz.navigation.bottom_navigation.BottomNavigationBar
 import com.hm.picplz.ui.screen.common.CommonTopBar
 import com.hm.picplz.ui.theme.MainFontFamily
@@ -47,6 +58,37 @@ fun ChatRoomScreen(
             modifier = modifier
                 .padding(innerPadding)
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.reservation_step_one),
+                    contentDescription = "레벨"
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 25.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "예약 대기",
+                        style = MainFontFamily.caption,
+                    )
+                    Text(
+                        text = "서비스 진행",
+                        style = MainFontFamily.caption
+                    )
+                    Text(
+                        text = "거래 확정",
+                        style = MainFontFamily.caption
+                    )
+                }
+            }
 
         }
     }
