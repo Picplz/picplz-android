ec20f81df60c935060327e9c9eb64669b69c906e
ldhldh07 <dhlim07@naver.com>
2025-04-22 19:47:57 +0900

    :lipstick: : 채팅 메세지 버블 프로필 추가 및 레이아웃 조정 - 파라미터를 챗 메세지 전체 받는것으로 수정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/chat/composable/ChatMessageBubble.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/chat/composable/ChatMessageBubble.kt
@@ -1,16 +1,35 @@
 package com.hm.picplz.ui.screen.chat.composable
 
+import android.net.Uri
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.hm.picplz.R
 import com.hm.picplz.data.model.User
+import com.hm.picplz.ui.model.ChatMessage
+import com.hm.picplz.ui.model.MessageContent
 import com.hm.picplz.ui.model.MessageDirection
 import com.hm.picplz.ui.theme.MainFontFamily.caption
 import com.hm.picplz.ui.theme.MainThemeColor
@@ -19,40 +38,60 @@ import com.hm.picplz.ui.theme.PicplzTheme
 @Composable
 fun ChatMessageBubble(
     modifier: Modifier = Modifier,
-    content: String = "",
-    direction: MessageDirection,
-    user: User,
+    chatMessage: ChatMessage,
 ){
-    Surface(
-        modifier = modifier
-            .padding(4.dp),
-        shape = RoundedCornerShape(20.dp),
-        color = when(direction) {
-            MessageDirection.SENT -> MainThemeColor.Gray1
-            MessageDirection.RECEIVED -> MainThemeColor.White
-        },
-        border = when(direction) {
-            MessageDirection.SENT -> null
-            MessageDirection.RECEIVED ->
-                BorderStroke(
-                    width = 1.dp,
-                    color = MainThemeColor.Gray3
-                )
-        }
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = if (chatMessage.direction === MessageDirection.RECEIVED) Arrangement.Start else Arrangement.End
     ) {
-        Text(
-            modifier = Modifier
-                .padding(
-                    horizontal = 11.dp,
-                    vertical = 10.dp,
-                )
-                .widthIn(
-                    max = 253.dp
-                )
-            ,
-            text = content,
-            style = caption,
-        )
+        if (chatMessage.direction === MessageDirection.RECEIVED) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(chatMessage.sender.profileImageUri ?: R.drawable.active_dot)
+                    .crossfade(true)
+                    .placeholder(R.drawable.active_dot)
+                    .error(R.drawable.active_dot)
+                    .build(),
+                contentDescription = "메세지 프로필",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+        }
+        Surface(
+            shape = RoundedCornerShape(20.dp),
+            color = when(chatMessage.direction) {
+                MessageDirection.SENT -> MainThemeColor.Gray1
+                MessageDirection.RECEIVED -> MainThemeColor.White
+            },
+            border = when(chatMessage.direction) {
+                MessageDirection.SENT -> null
+                MessageDirection.RECEIVED ->
+                    BorderStroke(
+                        width = 1.dp,
+                        color = MainThemeColor.Gray3
+                    )
+            }
+        ) {
+            val messageContent = chatMessage.content as MessageContent.Text
+
+            Text(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 11.dp,
+                        vertical = 10.dp,
+                    )
+                    .widthIn(
+                        max = 253.dp
+                    )
+                ,
+                text = messageContent.message,
+                style = caption,
+            )
+        }
     }
 }
 
@@ -61,12 +100,16 @@ fun ChatMessageBubble(
 fun ChatMessageSentBubblePreview() {
     PicplzTheme {
         ChatMessageBubble(
-            content = "말풍선 어쩌고 저쩌고 가로 최대 크기는 이만큼 입니다",
-            direction = MessageDirection.SENT,
-            user = User(
-                id = "1",
-                nickname = "나",
-                profileImageUri = null,
+            chatMessage = ChatMessage(
+                id = 1,
+                content = MessageContent.Text("말풍선 어쩌고 저쩌고 가로 최대 크기는 이만큼 입니다"),
+                direction = MessageDirection.SENT,
+                sender = User(
+                    id = "1",
+                    nickname = "나",
+                    profileImageUri = null,
+                ),
+                timestamp = (System.currentTimeMillis() - 100000).toString()
             )
         )
     }
@@ -77,13 +120,19 @@ fun ChatMessageSentBubblePreview() {
 fun ChatMessageReceivedBubblePreview() {
     PicplzTheme {
         ChatMessageBubble(
-            content = "말풍선 어쩌고 저쩌고 가로 최대 크기는 이만큼 입니다",
-            direction = MessageDirection.RECEIVED,
-            user = User(
-                id = "2",
-                nickname = "유가영 작가",
-                profileImageUri = null,
-            )
+            chatMessage = ChatMessage(
+                id = 2,
+                content = MessageContent.Text("말풍선 어쩌고 저쩌고 가로 최대 크기는 이만큼 입니다"),
+                direction = MessageDirection.RECEIVED,
+                sender = User(
+                    id = "2",
+                    nickname = "유가영 작가",
+                    profileImageUri = Uri.parse(
+                        "https://pbs.twimg.com/media/GlRFZh2akAA6KLR?format=jpg&name=large"
+                    )
+                ),
+                timestamp = (System.currentTimeMillis() - 100000).toString()
+            ),
         )
     }
 }
