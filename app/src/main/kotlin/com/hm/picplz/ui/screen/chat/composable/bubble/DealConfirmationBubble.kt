01205b1a062e8571e22b84402bb1ef25a15b3f03
ldhldh07 <dhlim07@naver.com>
2025-05-02 22:30:53 +0900

    :adhesive_bandage: : 기본 서식 수정

--- app/src/main/kotlin/com/hm/picplz/ui/screen/chat/composable/bubble/DealConfirmationBubble.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/chat/composable/bubble/DealConfirmationBubble.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -92,7 +91,6 @@ fun DealConfirmationBubble(
                     modifier = Modifier.fillMaxWidth()
                 )
             }
-
         }
     }
 }
