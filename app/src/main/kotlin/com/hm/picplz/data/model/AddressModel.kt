c12c1f95bb508a0039e92665168772328813c9cb
ldhldh07 <dhlim07@naver.com>
2025-05-26 16:45:44 +0900

    :sparkles: : 동 검색시 응답 및 요청 모델 정의

---
+++ app/src/main/kotlin/com/hm/picplz/data/model/AddressModel.kt
@@ -0,0 +1,19 @@
+package com.hm.picplz.data.model
+
+data class AreaSearchRequest(
+    val keyword: String,
+)
+
+data class AreaSearchResponse(
+    val timeStamp: String,
+    val statusCode: Int,
+    val message: String,
+    val data: List<AreaData>
+)
+
+data class AreaData(
+    val id: Int,
+    val name: String,
+    val dong: String,
+    val ri: String?,
+)
