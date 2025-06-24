a8342f3607de8ed32daff0d0e5304e143d4bc181
jungwon <wjddnjs0528@gmail.com>
2025-06-24 14:24:11 +0900

    ✨: 카카오소셜 로그인 SDK 추가 및 세팅

--- app/src/main/kotlin/com/hm/picplz/ui/screen/login/LoginIntroScreen.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/login/LoginIntroScreen.kt
@@ -1,8 +1,6 @@
 package com.hm.picplz.ui.screen.login
 
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
+import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
@@ -36,12 +34,14 @@ import com.hm.picplz.ui.screen.common.CommonHorizontalPager
 import com.hm.picplz.ui.theme.MainFontFamily
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.flow.collectLatest
 
 data class LoginIntroPageData(
-    val text: String,
-    @DrawableRes val imageRes: Int,
-    val isLast: Boolean = false // 마지막 페이지 여부
+    val text: String, @DrawableRes val imageRes: Int, val isLast: Boolean = false // 마지막 페이지 여부
 )
 
 object LoginIntroPageConfig {
@@ -54,10 +54,11 @@ object LoginIntroPageConfig {
     const val IMAGE_HEIGHT_FACTOR = 0.65f
 }
 
+val TAG = "LOGININTROSCREEN"
+
 @Composable
 fun LoginIntroScreen(
-    navController: NavController,
-    viewModel: LoginViewModel = viewModel()
+    navController: NavController, viewModel: LoginViewModel = viewModel()
 ) {
     val context = LocalContext.current
 
@@ -65,8 +66,7 @@ fun LoginIntroScreen(
     val imageHeight = screenHeight * LoginIntroPageConfig.IMAGE_HEIGHT_FACTOR
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        containerColor = MainThemeColor.White
+        modifier = Modifier.fillMaxSize(), containerColor = MainThemeColor.White
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -79,8 +79,7 @@ fun LoginIntroScreen(
                 showIndicator = true,
                 itemContent = { page ->
                     LoginIntroPage(
-                        page,
-                        imageHeight
+                        page, imageHeight
                     ) {
                         viewModel.handleIntent(LoginIntent.NavigateToKaKao)
                     }
@@ -100,32 +99,85 @@ fun LoginIntroScreen(
                      *  로그인 성공한 경우 -> 로그인 정보를 가지고 메인 페이지로 이동
                      *  로그인 정보가 없는 경우 -> 카카오와 연동된 회원가입 -> 회원가입 정보를 가지고 sign-up 스크린으로 이동
                      */
-                    Toast.makeText(context, "카카오 로그인", Toast.LENGTH_LONG).show()
-
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        navController.navigate("sign-up") {
-                            popUpTo(navController.graph.startDestinationId) {
-                                saveState = true
-                            }
-                            launchSingleTop = true
-                            restoreState = true
+                    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                        if (error != null) {
+                            Log.e(TAG, "카카오계정으로 로그인 실패", error)
+                        } else if (token != null) {
+                            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
                         }
-                    }, 500)
+                    }
+
+                    // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+                    if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                            if (error != null) {
+                                Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                                // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                                // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                                    return@loginWithKakaoTalk
+                                }
+
+                                // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                                UserApiClient.instance.loginWithKakaoAccount(
+                                    context, callback = callback
+                                )
+                            } else if (token != null) {
+                                requestKakaoLoginInfo(token)
+                            }
+                        }
+                    } else {
+                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    }
+
+
+//                    Toast.makeText(context, "카카오 로그인", Toast.LENGTH_LONG).show()
+//
+//                    Handler(Looper.getMainLooper()).postDelayed({
+//                        navController.navigate("sign-up") {
+//                            popUpTo(navController.graph.startDestinationId) {
+//                                saveState = true
+//                            }
+//                            launchSingleTop = true
+//                            restoreState = true
+//                        }
+//                    }, 500)
                 }
+
+                LoginSideEffect.LoginFailed -> TODO()
+                is LoginSideEffect.LoginSuccess -> TODO()
             }
         }
     }
 }
 
+private fun requestKakaoLoginInfo(token: OAuthToken) {
+    UserApiClient.instance.me { user, error ->
+        if (error != null) {
+            Log.e(TAG, "사용자 정보 요청 실패", error)
+            return@me
+        }
+
+        val accessToken = token.accessToken
+        val nickname = user?.kakaoAccount?.profile?.nickname
+        val profileImageUrl = user?.kakaoAccount?.profile?.thumbnailImageUrl
+
+        Log.i(
+            TAG, "사용자 정보 요청 성공" +
+                    "\n토큰: ${accessToken}" +
+                    "\n닉네임: ${nickname}" +
+                    "\n프로필사진: ${profileImageUrl}"
+        )
+    }
+}
+
 @Composable
 fun LoginIntroPage(
-    page: LoginIntroPageData,
-    imageHeight: Dp,
-    onButtonClick: () -> Unit
+    page: LoginIntroPageData, imageHeight: Dp, onButtonClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
             modifier = Modifier
@@ -162,6 +214,21 @@ fun LoginIntroPage(
                 containerColor = MainThemeColor.Yellow,
                 onClick = onButtonClick
             )
+            Spacer(modifier = Modifier.height(10.dp))
+            CommonBottomButton(
+                text = "카카오 로그아웃",
+                modifier = Modifier.padding(horizontal = 15.dp),
+                contentColor = MainThemeColor.Black,
+                containerColor = MainThemeColor.Yellow,
+                onClick = {
+                    UserApiClient.instance.unlink { error ->
+                        if (error != null) {
+                            Log.e(TAG, "연결 끊기 실패", error)
+                        } else {
+                            Log.i(TAG, "연결 끊기 성공 - 동의 항목 UI 다시 표시 가능")
+                        }
+                    }
+                })
         }
     }
 }
