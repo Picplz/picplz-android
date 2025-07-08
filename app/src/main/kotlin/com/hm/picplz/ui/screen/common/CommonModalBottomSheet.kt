347c557455cd294be7e6b916767468594966c847
jungwon <wjddnjs0528@gmail.com>
2025-07-08 19:48:31 +0900

    💄: CommonModalBottomSheet 로직 수정 - visibleCloseButton 변수 추가 (모달 자체에 닫기 버튼을 추가할지 말지 제어하는 변수)

--- app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonModalBottomSheet.kt
+++ app/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonModalBottomSheet.kt
@@ -1,11 +1,14 @@
 package com.hm.picplz.ui.screen.common
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -22,6 +25,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -30,11 +34,13 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
+import com.hm.picplz.R
 import com.hm.picplz.ui.theme.MainThemeColor
-import com.hm.picplz.utils.CommonUtil
+import kotlinx.coroutines.launch
 
 /**
  * ModalBottomSheet를 래핑한 컴포저블 함수로, 모달 형태의 바텀시트.
@@ -42,6 +48,8 @@ import com.hm.picplz.utils.CommonUtil
  *
  * @param modifier [Modifier] : 이 컴포넌트에 적용할 Modifier
  * @param onDismissRequest [() -> Unit] : 바텀시트를 닫을 때 호출되는 콜백
+ * @param visible [Boolean] : 바텀시트를 열고 닫기를 제어하는 변수
+ * @param visibleCloseButton [Boolean] : 닫기 버튼 아이콘을 포함할지 제어하는 변수
  * @param sheetState [SheetState] : 바텀시트의 상태를 관리하는 객체
  * @param sheetMaxWidth [Dp] : 바텀시트의 최대 너비. 기본값은 BottomSheetDefaults.SheetMaxWidth
  * @param shape [Shape] : 바텀시트의 모서리 둥글기. 기본값은 RoundedCornerShape(20.dp, 20.dp)
@@ -62,6 +70,7 @@ fun CommonModalBottomSheet(
     modifier: Modifier = Modifier,
     onDismissRequest: () -> Unit,
     visible: Boolean = true,
+    visibleCloseButton: Boolean = false,
     sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
     sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
     shape: Shape = RoundedCornerShape(20.dp, 20.dp),
@@ -91,6 +100,7 @@ fun CommonModalBottomSheet(
             }
         }
     }
+    val scope = rememberCoroutineScope()
 
     if (visible) {
         ModalBottomSheet(
@@ -110,21 +120,34 @@ fun CommonModalBottomSheet(
                 modifier = Modifier
                     .fillMaxWidth()
                     .nestedScroll(connection)
-
+                    .heightIn(
+                        min = sheetMinHeight ?: 0.dp,
+                        max = sheetMaxHeight ?: Dp.Infinity
+                    )
+                    .fillMaxHeight(0.9f)
+                    .background(Color.Red)
             ) {
-                Column(
-                    modifier = CommonUtil
-                        .paddingModifier
-                        .heightIn(
-                            min = sheetMinHeight ?: 0.dp,
-                            max = sheetMaxHeight ?: Dp.Infinity
-                        )
-                        .fillMaxHeight(0.9f)
-                ) {
-                    Spacer(modifier = Modifier.height(20.dp))
+                // ✅ 메인 콘텐츠
+                Column(modifier = Modifier.fillMaxSize()) {
                     content()
                 }
 
+                // ✅ 오른쪽 상단에 겹쳐지는 닫기 버튼
+                if (visibleCloseButton) {
+                    Image(
+                        painter = painterResource(id = R.drawable.modal_button_close),
+                        contentDescription = "close-button",
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(top = 17.5.dp, end = 15.dp)
+                            .clickable {
+                                scope.launch {
+                                    sheetState.hide() // 👈 애니메이션으로 바텀시트 숨김
+                                    onDismissRequest() // 👈 숨김 완료 후 상태 갱신
+                                }
+                            }
+                    )
+                }
             }
         }
     }
