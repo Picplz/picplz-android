67e831757eefe68f1c23a150d196b7f66000fd63
ldhldh07 <dhlim07@naver.com>
2026-01-19 19:50:30 +0900

    :recycle: : AuthService 역할 축소 - 백엔드 API 전용으로 변경

--- app/src/main/kotlin/com/hm/picplz/data/service/AuthService.kt
+++ app/src/main/kotlin/com/hm/picplz/data/service/AuthService.kt
@@ -1,16 +1,11 @@
 package com.hm.picplz.data.service
 
 import com.hm.picplz.data.model.KaKaoLoginResponse
-import com.hm.picplz.data.model.KakaoUserInfo
 import com.hm.picplz.data.source.AuthSource
-import com.kakao.sdk.user.UserApiClient
-import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
-import kotlin.coroutines.resume
 
 interface AuthService {
     suspend fun loginWithKaKao(accessToken: String): Result<KaKaoLoginResponse>
-    suspend fun getKakaoUserInfo(): Result<KakaoUserInfo>
 }
 
 class AuthServiceImpl @Inject constructor(
@@ -19,16 +14,4 @@ class AuthServiceImpl @Inject constructor(
     override suspend fun loginWithKaKao(accessToken: String): Result<KaKaoLoginResponse> {
         return authSource.loginWithKaKao(accessToken = accessToken)
     }
-
-    override suspend fun getKakaoUserInfo(): Result<KakaoUserInfo> =
-        suspendCancellableCoroutine { cont ->
-            UserApiClient.instance.me { user, error ->
-                if (error != null) {
-                    cont.resume(Result.failure(error))
-                } else {
-                    val profileImageUrl = user?.kakaoAccount?.profile?.profileImageUrl
-                    cont.resume(Result.success(KakaoUserInfo(profileImageUrl = profileImageUrl)))
-                }
-            }
-        }
 }
