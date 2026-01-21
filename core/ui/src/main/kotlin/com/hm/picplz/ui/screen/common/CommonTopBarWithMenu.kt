ab4f8cfd6d09d039964e986f348146cdca66b548
ldhldh07 <dhlim07@naver.com>
2026-01-22 00:35:59 +0900

    ♻️ CommonTopBar 컴포넌트 파일 분리
    - CommonTopBar.kt: 기본 TopBar + FixedTopBar + 공유 상수
    - CommonTopBarWithMenu.kt: 메뉴 아이콘 버전
    - CommonTopBarWithSubtitle.kt: 부제목 버전

--- core/ui/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBarWithMenu.kt
+++ core/ui/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBarWithMenu.kt
@@ -1,13 +1,13 @@
 package com.hm.picplz.ui.screen.common
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,21 +15,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.hm.picplz.core.ui.R
-import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
 import com.hm.picplz.ui.theme.pretendardTypography
 
-internal val TopBarHeight = 44.dp
-internal val TopBarHorizontalPadding = 16.dp
-internal val TopBarIconSize = 18.dp
-
 @Composable
-fun CommonTopBar(
+fun CommonTopBarWithMenu(
     text: String,
     onClickBack: () -> Unit,
+    onClickMenu: () -> Unit,
 ) {
     Row(
         modifier =
@@ -62,37 +56,30 @@ fun CommonTopBar(
             )
         }
 
-        Box(modifier = Modifier.weight(1f))
-    }
-}
-
-@Composable
-fun CommonFixedTopBar(
-    title: String,
-    onClickBack: () -> Unit,
-) {
-    Box(
-        modifier =
-            Modifier
-                .background(MainThemeColor.White)
-                .zIndex(1f)
-                .height(TopBarHeight),
-    ) {
-        CommonTopBar(
-            text = title,
-            onClickBack = onClickBack,
-        )
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.CenterEnd,
+        ) {
+            IconButton(onClick = onClickMenu) {
+                Icon(
+                    painter = painterResource(id = R.drawable.menu),
+                    contentDescription = "메뉴",
+                    modifier = Modifier.size(TopBarIconSize),
+                )
+            }
+        }
     }
 }
 
 @Suppress("UnusedPrivateMember")
 @Preview(showBackground = true)
 @Composable
-private fun CommonTopBarPreview() {
+private fun CommonTopBarWithMenuPreview() {
     PicplzTheme {
-        CommonTopBar(
+        CommonTopBarWithMenu(
             text = "제목",
             onClickBack = {},
+            onClickMenu = {},
         )
     }
 }
--- core/ui/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBarWithSubtitle.kt
+++ core/ui/src/main/kotlin/com/hm/picplz/ui/screen/common/CommonTopBarWithSubtitle.kt
@@ -1,35 +1,36 @@
 package com.hm.picplz.ui.screen.common
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.hm.picplz.core.ui.R
+import com.hm.picplz.ui.theme.MainFontFamily
 import com.hm.picplz.ui.theme.MainThemeColor
 import com.hm.picplz.ui.theme.PicplzTheme
 import com.hm.picplz.ui.theme.pretendardTypography
 
-internal val TopBarHeight = 44.dp
-internal val TopBarHorizontalPadding = 16.dp
-internal val TopBarIconSize = 18.dp
-
 @Composable
-fun CommonTopBar(
+fun CommonTopBarWithSubtitle(
     text: String,
+    subText: String,
     onClickBack: () -> Unit,
+    onClickMenu: () -> Unit,
+    subTextStyle: TextStyle = MainFontFamily.caption,
 ) {
     Row(
         modifier =
@@ -56,43 +57,47 @@ fun CommonTopBar(
             modifier = Modifier.weight(1f),
             contentAlignment = Alignment.Center,
         ) {
-            Text(
-                text = text,
-                style = pretendardTypography.bodyMedium,
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = text,
+                    style = pretendardTypography.bodyMedium,
+                )
+                Text(
+                    text = subText,
+                    style = subTextStyle,
+                    modifier = Modifier.padding(top = 2.dp),
+                )
+            }
         }
 
-        Box(modifier = Modifier.weight(1f))
-    }
-}
-
-@Composable
-fun CommonFixedTopBar(
-    title: String,
-    onClickBack: () -> Unit,
-) {
-    Box(
-        modifier =
-            Modifier
-                .background(MainThemeColor.White)
-                .zIndex(1f)
-                .height(TopBarHeight),
-    ) {
-        CommonTopBar(
-            text = title,
-            onClickBack = onClickBack,
-        )
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.CenterEnd,
+        ) {
+            IconButton(onClick = onClickMenu) {
+                Icon(
+                    painter = painterResource(id = R.drawable.menu),
+                    contentDescription = "메뉴",
+                    modifier = Modifier.size(TopBarIconSize),
+                )
+            }
+        }
     }
 }
 
 @Suppress("UnusedPrivateMember")
 @Preview(showBackground = true)
 @Composable
-private fun CommonTopBarPreview() {
+private fun CommonTopBarWithSubtitlePreview() {
     PicplzTheme {
-        CommonTopBar(
-            text = "제목",
+        CommonTopBarWithSubtitle(
+            text = "유가영 작가",
+            subText = "당장 촬영 가능",
+            subTextStyle = MainFontFamily.caption.copy(color = MainThemeColor.Green120),
             onClickBack = {},
+            onClickMenu = {},
         )
     }
 }
