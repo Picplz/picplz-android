941b370e0d180eeda66ef0ce5d71eab3d17cad01
ldhldh07 <dhlim07@naver.com>
2026-01-21 22:13:17 +0900

    ♻️ NavigationRoute sealed interface 도입으로 type-safe navigation 강화

--- feature/auth/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_common/SignUpCommonSideEffect.kt
+++ feature/auth/src/main/kotlin/com/hm/picplz/ui/screen/sign_up/sign_up_common/SignUpCommonSideEffect.kt
@@ -1,6 +1,7 @@
 package com.hm.picplz.ui.screen.sign_up.sign_up_common
 
 import com.hm.picplz.common.model.User
+import com.hm.picplz.navigation.model.NavigationRoute
 
 sealed interface SignUpSideEffect {
     sealed interface SelectUserTypeScreenSideEffect : SignUpSideEffect {
@@ -12,7 +13,7 @@ sealed interface SignUpSideEffect {
 
     data object NavigateToPrev : SignUpSideEffect
 
-    data class Navigate(val destination: Any) : SignUpSideEffect
+    data class Navigate(val destination: NavigationRoute) : SignUpSideEffect
 
     data object ShowFileUploadDialog : SignUpSideEffect
 }
