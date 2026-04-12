62b3b6d223aac4e24451d2cad4ea8a1168eb9798
eshc123 <eshc.joon@gmail.com>
2026-04-12 11:04:25 +0900

    ✨ 예약 취소 페이지 기본 구성 파일 생성

---
+++ feature/reservation/src/main/kotlin/com/hm/picplz/ui/screen/cancel_reservation/CancelReservationScreen.kt
@@ -0,0 +1,13 @@
+package com.hm.picplz.ui.screen.cancel_reservation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CancelReservationScreen() {}
+
+@Preview
+@Composable
+private fun CancelReservationScreenPreview() {
+    CancelReservationScreen()
+}
